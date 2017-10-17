cdae34ae6b308840a15dc83732af7362fc56109b
Yingcai Dong <dyc1992@gmail.com>
2017-10-17 01:45:55 -0400

    Improve search intake volume

--- app/employee-list/employee-list.component.js
+++ app/employee-list/employee-list.component.js
@@ -9,25 +9,19 @@ angular.
       var self = this;
       self.orderProp = 'emp_no';
 
-      $http.get('http://localhost:8080/employees/range/10001to10020').then(function(response) {
+      $http.get('http://localhost:8080/employees/range/10001to10100').then(function(response) {
         self.employees = response.data;
-        console.log(self.employees);
       });
 
       var API_KEY = '6728169-c144c00092689f1b51c8f3c98';
-      var url = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('man');
+      var url = "https://pixabay.com/api/?key="+API_KEY+"&safesearch=true&per_page=100&q="+encodeURIComponent('emoji');
       self.thumbnail = [];
       $http.get(url).then(function (response) {
           var dataReceived = response.data;
           if (parseInt(dataReceived.totalHits) > 0) {
-
-            console.log(dataReceived.totalHits+1);
-            console.log(dataReceived.hits);
-
             var values = dataReceived.hits;
             angular.forEach(values, function (value, key) {
                 self.thumbnail.push(value.previewURL);
-                // console.log(value.previewURL);
             })
           }
       })
